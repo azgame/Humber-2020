dc76e2ce5eef559fe9c597308f14a84b2cca6413
azizys <36770253+azizys@users.noreply.github.com>
2019-09-16 17:10:37 -0400

    lab 1
    finished animations and logic for powerup/death
    Need to add shaders

--- Engines 2/Engines2/Assets/Scripts/Character.cs
+++ Engines 2/Engines2/Assets/Scripts/Character.cs
@@ -2,40 +2,118 @@
 using System.Collections;
 
 public class Character : MonoBehaviour {
-    CharacterController characterController;
 
-    public float speed = 6.0f;
-    public float jumpSpeed = 8.0f;
-    public float gravity = 20.0f;
 
-    private Vector3 moveDirection = Vector3.zero;
-    private Animator animator;
+    public enum m_AnimationStates {
+        Idle,
+        Walking,
+        Jump,
+        Hit,
+        Attack,
+        PowerUp,
+        Death
+    }
+
+  
+    public float                        speed;
+    public float                        jumpSpeed;
+    public float                        gravity;
+
+    private bool                        dead = false;
+    private int                         junkFood = 0;
+    private Vector3                     moveDirection = Vector3.zero;
+    private Animator                    animator;
+    private CharacterController         characterController;
+
 
     void Start() {
+
         characterController = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
     }
 
     void Update() {
+
         if (characterController.isGrounded) {
-            // We are grounded, so recalculate
-            // move direction directly from axes
 
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
             moveDirection *= speed;
 
+            SetAnimationState(m_AnimationStates.Walking);
+
             if (Input.GetButton("Jump")) {
                 moveDirection.y = jumpSpeed;
-                animator.SetTrigger("Jump");
+                SetAnimationState(m_AnimationStates.Jump);
+            }
+
+            if (Input.GetButton("Fire1")) {
+                SetAnimationState(m_AnimationStates.Attack);
             }
         }
 
-        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
-        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
-        // as an acceleration (ms^-2)
+        if (dead)
+            moveDirection = new Vector3(0.0f, moveDirection.y, 0.0f);
+
         moveDirection.y -= gravity * Time.deltaTime;
-        
-        // Move the controller
         characterController.Move(moveDirection * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider hit) {
+        switch (hit.gameObject.tag) {
+            case "Death":
+                Death(hit);
+                SetAnimationState(m_AnimationStates.Death);
+                break;
+            case "PowerUp":
+                CollectFood(hit);
+                SetAnimationState(m_AnimationStates.PowerUp);
+                break;
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Set animations here
+    /// Run any animation dependant code here
+    /// </summary>
+    void SetAnimationState(m_AnimationStates anim) {
+
+        switch (anim) {
+            case m_AnimationStates.Idle:
+                break;
+            case m_AnimationStates.Walking:
+                animator.SetFloat("Speed", Mathf.Abs(moveDirection.x) + Mathf.Abs(moveDirection.z));
+                break;
+            case m_AnimationStates.Jump:
+                animator.SetTrigger("Jump");
+                break;
+            case m_AnimationStates.Hit:
+                animator.SetTrigger("Hit");
+                break;
+            case m_AnimationStates.Attack:
+                animator.SetTrigger("Attack");
+                break;
+            case m_AnimationStates.PowerUp:
+                animator.SetTrigger("PowerUp");
+                break;
+            case m_AnimationStates.Death:
+                animator.SetTrigger("Death");
+                break;
+            default:
+                break;
+        }
+    }
+
+    void Death(Collider hit) {
+        dead = true;
+        Debug.Log("You died");
+        Destroy(hit.gameObject);
+    }
+
+    void CollectFood(Collider hit) {
+        junkFood++;
+        Debug.Log("You have " + junkFood + " junk foods!");
+        Destroy(hit.gameObject);
     }
 }
