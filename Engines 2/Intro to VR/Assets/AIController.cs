63b3077addc477efc380ce44c3647d2c5d3ccc78
azizys <36770253+azizys@users.noreply.github.com>
2019-09-19 20:32:31 -0400

    unity vr

---
+++ Engines 2/Intro to VR/Assets/AIController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIController : MonoBehaviour {
+
+    public GameObject target;
+    private NavMeshAgent nmAgent;
+	
+	
+    void Start() {
+
+        nmAgent = GetComponent<NavMeshAgent>();
+    }
+	
+	
+    void Update() {
+
+        if (target)
+            nmAgent.SetDestination(target.transform.position);
+    }
+}
