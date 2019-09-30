259c2371ac7867ef3feddc950557a524ea71c7cc
azizys <36770253+azizys@users.noreply.github.com>
2019-09-30 11:10:08 -0400

    updates to engines 2 repo

---
+++ Engines 2/LAB1_ZIZYS_AIDAN/Assets/Scripts/VertexDisp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VertexDisp : MonoBehaviour {
+
+    MeshRenderer meshRender;
+    float displacementAmount = 0;
+
+    void Start() {
+        meshRender = GetComponent<MeshRenderer>();
+    }
+
+    void Update() {
+        if (Input.GetButtonDown("Jump"))
+            displacementAmount++;
+
+        displacementAmount = Mathf.Lerp(displacementAmount, 0, Time.deltaTime);
+        meshRender.material.SetFloat("_Amount", displacementAmount);
+    }
+}
